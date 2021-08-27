16a55fdd87e25d19f921815470fa6f883c268c30
TakumaShiga <tak07sweetvo@gmail.com>
2021-08-27 21:23:20 +0900

    最終リファクタリング

--- brave.rb
+++ brave.rb
@@ -39,7 +39,6 @@ class Brave < Character
     damage = params[:damage]
     target.hp -= damage
     target.hp = 0 if target.hp < 0
-    # puts "#{target.name}は#{damage}のダメージを受けた"
   end
 
   def calculate_special_attack
--- main2.rb
+++ main2.rb
@@ -2,10 +2,15 @@ require "./brave"
 require "./monster"
 require "./games_controller"
 
+games_controller = GamesController.new
 
-monster = Monster.new(name: "スライム", hp: 250, offense: 200, defense: 100)
+slime = Monster.new(name: "スライム", hp: 250, offense: 200, defense: 100)
 
-brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
+terry = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
 
+games_controller.battle(brave: terry, monster: slime)
+
+EXP_CONSTANT = 2
+GOLD_CONSTANT = 3
 
 
